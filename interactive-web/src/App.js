8316b8f2dd730ea6c5d421d0be8979496889b1c8
Zheng <lidan.zheng6@gmail.com>
2021-04-09 14:01:13 -0400

    change title and content

--- interactive-web/src/App.js
+++ interactive-web/src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello! You can watch the youtube video to learn!
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          className="youtube-link"
+          href="https://youtu.be/I2UBjN5ER4s"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          We need to start!
         </a>
       </header>
     </div>
