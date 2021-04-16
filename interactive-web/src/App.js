67aa8bc7f519f85d2346d21e6f215a38530b9a8c
Zheng <lidan.zheng6@gmail.com>
2021-04-16 00:17:55 -0400

    remove compiler warning

--- interactive-web/src/App.js
+++ interactive-web/src/App.js
@@ -1,8 +1,8 @@
 
 import './App.css';
-import NavigationBar from './component/NavigationBar';
-import Footer from './component/Footer';
-import DonationHeader from './component/DonationHeader';
+// import NavigationBar from './component/NavigationBar';
+// import Footer from './component/Footer';
+// import DonationHeader from './component/DonationHeader';
 import DonationPage from './pages/DonationPage';
 
 function App() {
