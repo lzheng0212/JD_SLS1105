71aa8247d9322b286f36d019f0e09009e679e014
Zheng <lidan.zheng6@gmail.com>
2021-04-16 13:00:17 -0400

    added welcomeSub for compilation

--- interactive-web/src/pages/Home.js
+++ interactive-web/src/pages/Home.js
@@ -2,12 +2,14 @@ import '../App.css';
 import WelcomeSection from '../component/WelcomeSection';
 import Footer from '../component/Footer';
 import WelcomeSub from '../component/WelcomeSub';
+import Posts from '../component/Posts';
 
 function Home() {
     return (
         <>
             <WelcomeSection />
             <WelcomeSub />
+            <Posts />
             <Footer />
         </>
     );
