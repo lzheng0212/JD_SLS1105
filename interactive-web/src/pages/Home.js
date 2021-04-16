23797962c01f1a27e35b45824eb7fa6c9c68325a
Zheng <lidan.zheng6@gmail.com>
2021-04-16 02:44:09 -0400

    Welcome screen and fixed navbar

---
+++ interactive-web/src/pages/Home.js
@@ -0,0 +1,12 @@
+import '../App.css';
+import WelcomeSection from '../component/WelcomeSection';
+
+function Home() {
+    return (
+        <>
+            <WelcomeSection />
+        </>
+    );
+}
+
+export default Home;
