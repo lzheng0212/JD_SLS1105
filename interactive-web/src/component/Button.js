0c5261f68b02ba31e364b647af320b056897cafa
Zheng <lidan.zheng6@gmail.com>
2021-04-16 04:15:45 -0400

    modify home page

--- interactive-web/src/component/Button.js
+++ interactive-web/src/component/Button.js
@@ -14,16 +14,18 @@ import React from 'react';
 import './Button.css';
 import { Link } from 'react-router-dom';
 
-const STYLES = ['btn--primary', 'btn--outline', 'btn--test'];
+const STYLES = ['btn--primary', 'btn--outline', 'btn--round'];
 
 const SIZES = ['btn--medium', 'btn--large'];
+
 
 export const Button = ({
   children,
   type,
   onClick,
   buttonStyle,
-  buttonSize
+  buttonSize,
+  color
 }) => {
   const checkButtonStyle = STYLES.includes(buttonStyle)
     ? buttonStyle
@@ -31,12 +33,14 @@ export const Button = ({
 
   const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
 
+
   return (
     <Link to='/' className='btn-mobile'>
       <button
         className={`btn ${checkButtonStyle} ${checkButtonSize}`}
         onClick={onClick}
         type={type}
+        color={color}
       >
         {children}
       </button>
--- interactive-web/src/pages/Home.js
+++ interactive-web/src/pages/Home.js
@@ -1,11 +1,13 @@
 import '../App.css';
 import WelcomeSection from '../component/WelcomeSection';
 import Footer from '../component/Footer';
+import WelcomeSub from '../component/WelcomeSub';
 
 function Home() {
     return (
         <>
             <WelcomeSection />
+            <WelcomeSub />
             <Footer />
         </>
     );
