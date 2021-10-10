29b49506d760e9935f027e3e8583e65d864cb97e
Lidan Zheng <65567426+lzheng0212@users.noreply.github.com>
2021-10-10 00:56:01 -0400

    merge

--- interactive-web/src/App.js
+++ interactive-web/src/App.js
@@ -3,6 +3,7 @@ import DonationPage from "./pages/DonationPage";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import PostSection from "./pages/postSection";
+import MapPage from "./pages/MapPage";
 import React from "react";
 import aboutUsPage from "./pages/aboutUsPage";
 import FullCalendarPage from "./pages/FullCalendarPage";
@@ -29,6 +30,7 @@ function App() {
         <Route path="/aboutUs" exact component={aboutUsPage} />
         <Route path="/adminLogin" exact component={AdminLoginPage} />
         <Route path="/adminportal" exact component={AdminPortal} />
+        <Route path="/map" exact component={MapPage} />
         <Route path="/events" exact component={FullCalendarPage} />
         <Route path="/specificPost" exact component={SpecificPost} />
         <Route path="/timeline" exact component={TimelinePage} />
