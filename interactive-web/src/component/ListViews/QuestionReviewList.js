ccf868d5417fa9a1d83e9a0b3bf1cfcd470e3d2a
Lai <lai@gatech.edu>
2021-11-30 00:19:30 -0500

    fix style of faqList and TimelineList

--- interactive-web/src/component/ListViews/QuestionReviewList.js
+++ interactive-web/src/component/ListViews/QuestionReviewList.js
@@ -1,10 +1,11 @@
 import React, { useEffect } from 'react'
-import { List, Popconfirm, Space, Col, Button, Row, Layout, message } from 'antd'
+import { List, Popconfirm, Col, Button, Row, message, Input } from 'antd'
 import { projectFirestore } from '../../firebase/config'
 import './PostList.css'
 
 export default function QuestionReviewList () {
   const [events, setEvents] = React.useState([])
+  const { TextArea } = Input
 
   useEffect(() => {
     projectFirestore.collection('CustomerQuestions').orderBy('createdAt', 'desc').onSnapshot((snap) => {
@@ -32,80 +33,81 @@ export default function QuestionReviewList () {
     document.getElementById('answer').value = ''
   }
 
-  return (
-        <Layout>
-            <input
+  const buttonWrapper = () => (
+    <Row md={{ justify: 'center' }} style={{ padding: '24px' }} gutter={ [8, 8] }>
+        <Col span={24}>
+          <Input
                 placeholder="Question"
                 type="text"
                 id="question"
                 name="question"
                 style={{ width: '100%' }}
             />
-            <input
+        </Col>
+        <Col span={24}>
+            <TextArea
                 placeholder="Answer"
                 type="text"
                 id="answer"
                 name="answer"
                 style={{ width: '100%' }}
             />
-            <Button type="primary" onClick={postQuestion} style={{ backgroundColor: '#234144', borderColor: '#1a3133' }}>
+        </Col>
+        <Col span={24}>
+            <Button type="primary" onClick={postQuestion} style={{ backgroundColor: '#234144', borderColor: '#1a3133', float: 'right' }}>
                 Post It On FAQ
             </Button>
-            <List
-                itemLayout="vertical"
-                size="large"
-                pagination={{
-                  onChange: page => {
-                    console.log(page)
-                  },
-                  pageSize: 5
-                }}
-                dataSource={events}
-                style={{ padding: '12px 24px 24px', backgroundColor: 'white' }}
-                // bordered="true"
-                renderItem={item => (
-                    <List.Item
-                        key={item.email}
-                        extra={
-                            <Space style={{ marginLeft: '-8px' }}>
+        </Col>
+    </Row>
+  )
 
-                                    <Row justify="end" align='middle' gutter={8}>
-                                    <Col xs={0} sm={0} md={0} lg={12}>
-                                        <Popconfirm
-                                        cancelButtonProps={{ type: 'primary' }}
-                                        okType={{ type: 'default' }}
-                                        title="Are you sure to delete this question?"
-                                        onConfirm={() => deleteQuestion(item)}
-                                        onVisibleChange={() => console.log('Question deleted')}
-                                        >
-                                            <Button type="primary" danger style={{ width: '80px' }}>
-                                                DELETE
-                                            </Button>
-                                        </Popconfirm>
-
-                                    </Col>
-
-                                </Row>
-                            </Space>
-                        }
+  return (
+        <List
+          itemLayout="vertical"
+          size="large"
+          pagination={{
+            onChange: page => {
+              console.log(page)
+            },
+            pageSize: 5
+          }}
+          dataSource={events}
+          style={{ padding: '12px 24px 24px', backgroundColor: 'white' }}
+          header={buttonWrapper()}
+          // bordered="true"
+          renderItem={item => (
+              <List.Item
+                  key={item.email}
+                  extra={
+                    <Popconfirm
+                      cancelButtonProps={{ type: 'primary' }}
+                      okType={{ type: 'default' }}
+                      title="Are you sure to delete this question?"
+                      onConfirm={() => deleteQuestion(item)}
+                      onVisibleChange={() => console.log('Question deleted')}
                     >
+                      <Button type="primary" danger style={{ width: '80px' }}>
+                          DELETE
+                      </Button>
+                    </Popconfirm>
+                  }
+              >
 
-                    <List.Item.Meta
-                        title={item.question}
-                        description={
-                        <>
-                            <span> Name: </span>{item.name}
-                            <p/>
-                            <span> Email: </span>{item.email}
-                            <p/>
-                        </>
-                        }
-                    />
+              <List.Item.Meta
+                  title={item.question}
+                  description={
+                  <>
+                      <span> Name: </span>{item.name}
+                      <p/>
+                      <span> Email: </span>{item.email}
+                      <p/>
+                  </>
+                  }
+              />
 
-                    </List.Item>
+              </List.Item>
 
-                )}
-            />
-        </Layout>
+          )}
+      />
   )
 }
